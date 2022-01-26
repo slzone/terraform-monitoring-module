97d6128781171bb9bce4f345013402e27fe335c7
ERNESTO DE AVILA <edeavil@us.ibm.com>
2022-01-26 14:34:50 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,13 +2,11 @@ variable "ibmcloud_api_key" {
     description = "apikey"
 }
 
-variable "region" {
-  default = "us-east"  
+variable "region" { 
   description = "Region to provision the Openshift cluster. List all available regions with: ibmcloud regions"
 }
 
 variable "resource_group" {
-  default = "edeavila-wrkld-vpc"
   description = "Resource Group id in your account to host the cluster. List all available resource groups with: ibmcloud resource groups. It is created if empty"
 }
 
@@ -20,11 +18,11 @@ variable "resource_group" {
     #description = "Monitoring Cluster"
 #}
 
-variable "wrkld_monitoring_worker_pools" {
-  description = "Entitlement for the cluster"
-  type    = any
-  default = {}
-}
+#variable "wrkld_monitoring_worker_pools" {
+  #description = "Entitlement for the cluster"
+  #type    = any
+  #default = {}
+#}
 
 #variable "resource_group_id" {
     #type = string
